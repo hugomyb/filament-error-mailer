595a3236d9f6279abb333f127e51dbbf1d53b03c
Mayonobe Hugo <hugomayonobe@gmail.com>
2024-03-17 20:10:36 +0100

    [+] (view) add request infos and auth user infos

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -14,6 +14,41 @@
 ## URL :
 # {{ url(request()->getPathInfo()) ?? "" }}
 
+@component('mail::table')
+| Request : | |
+| --- | --- |
+| **Method** | {{ request()->method() }} |
+| **IP** | {{ request()->ip() }} |
+| **User Agent** | {{ request()->userAgent() }} |
+| **Referrer** | {{ request()->header('referer') }} |
+| **Request Time** | {{ \Carbon\Carbon::createFromTimestamp(request()->server('REQUEST_TIME'))->toDateTimeString() }} |
+| **Request URI** | {{ request()->server('REQUEST_URI') }} |
+@endcomponent
+
+@if(auth()->check())
+@component('mail::table')
+| Auth User : | |
+| --- | --- |
+| **ID** | {{ auth()->id() }} |
+| **Name** | {{ auth()->user()->name ?? "" }} |
+| **Email** | {{ auth()->user()->email ?? "" }} |
+@endcomponent
+@else
+@component('mail::table')
+| Auth User : | |
+| --- | --- |
+| No Auth User | |
+@endcomponent
+@endif
+
 ## Trace :
+@component('mail::panel')
 {{ $exception || $stackTrace ? $stackTrace : "" }}
+@endcomponent
 </x-mail::message>
+
+<style>
+thead > tr > th {
+text-align: left;
+}
+</style>
