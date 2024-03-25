e1521704ab355a214dd191d6d03f7abbcbc33d15
erenilhan <erenilhan@ss2i-digital.fr>
2024-03-25 13:40:44 +0300

    removed default values

--- config/error-mailer.php
+++ config/error-mailer.php
@@ -3,8 +3,8 @@
 return [
     'email' => [
         'recipient' => ['recipient1@example.com', 'recipient2@example.com'],
-        'bcc' => ['bcc1@example.com', 'bcc2@example.com'],
-        'cc' => ['cc1@example.com', 'cc2@example.com'],
+        'bcc' => [],
+        'cc' => [],
         'subject' => 'An error was occured - ' . env('APP_NAME'),
     ],
 
